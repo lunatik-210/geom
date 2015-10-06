e03e3096aacab842245e100ef34abf0a70a32730
Andrew Lapin <andrew.d.lapin@gmail.com>
2015-10-06 20:45:45 +0300

    Add more tests on Line and intersections

--- src/app/tools/geom/gcore.spec.js
+++ src/app/tools/geom/gcore.spec.js
@@ -2,9 +2,32 @@ import * as gcore from './gcore';
 
 describe('Line properties', () => {
     it('Line should have A == 0 when parallel to Ox', () => {
-        let l1 = gcore.createLineByPoints(new gcore.Point(2, 2), new gcore.Point(5, 2));
-        expect(l1.a === 0).toBeTruthy();
-        expect(l1.b !== 0).toBeTruthy();
-        expect(l1.c !== 0).toBeTruthy();
+        let l = gcore.createLineByPoints(new gcore.Point(2, 2), new gcore.Point(5, 2));
+        expect(l.a === 0).toBeTruthy();
+        expect(l.b !== 0).toBeTruthy();
+        expect(l.c !== 0).toBeTruthy();
+    });
+
+    it('Line should have B == 0 when parallel to Oy', () => {
+        let l = gcore.createLineByPoints(new gcore.Point(2, 5), new gcore.Point(2, 2));
+        expect(l.a !== 0).toBeTruthy();
+        expect(l.b === 0).toBeTruthy();
+        expect(l.c !== 0).toBeTruthy();
+    });
+
+    it('Line should have С == 0 when goes through (0,0)', () => {
+        let l = gcore.createLineByPoints(new gcore.Point(1, 1), new gcore.Point(2, 2));
+        expect(l.a !== 0).toBeTruthy();
+        expect(l.b !== 0).toBeTruthy();
+        expect(l.c === 0).toBeTruthy();
+    });
+});
+
+describe('Line intersection point', () => {
+    it('Two lines should intersect in the point of (3,3)', () => {
+        let l1 = gcore.createLineByPoints(new gcore.Point(1, 1), new gcore.Point(2, 2));
+        let l2 = gcore.createLineByPoints(new gcore.Point(3, 0), new gcore.Point(3, 8));
+        let intersection = l1.intersectionPoint(l2);
+        expect(intersection.isEqual(new gcore.Point(3,3)));
     });
 });
