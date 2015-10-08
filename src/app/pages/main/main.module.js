36bd3716c82699fe9bb5a745fa0d7950d96f0998
Andrew Lapin <andrew.d.lapin@gmail.com>
2015-10-09 02:53:55 +0300

    Add animation for reset button

--- src/app/pages/main/main.module.js
+++ src/app/pages/main/main.module.js
@@ -3,6 +3,6 @@ import MainController from './main.controller';
 
 import './../../components/konvastage/konvastage.module';
 
-angular.module('app.pages.main', ['ui.router', 'app.components.konvastage'])
+angular.module('app.pages.main', ['ngAnimate', 'ui.router', 'app.components.konvastage'])
   .config(routerConfig)
   .controller('MainController', MainController);
