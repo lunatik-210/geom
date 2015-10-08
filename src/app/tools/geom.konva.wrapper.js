fd1b278cf3aabf18ccaf535808a9040d78ba329d
Andrew Lapin <andrew.d.lapin@gmail.com>
2015-10-09 02:39:02 +0300

    Add shadow for models

--- src/app/tools/geom.konva.wrapper.js
+++ src/app/tools/geom.konva.wrapper.js
@@ -3,19 +3,29 @@ import {Point, Circle, Parallelogram} from './geom/gcore';
 
 const STROKE_WIDTH = 2;
 
+const SHADOW = {
+    shadowColor: 'black',
+    shadowBlur: 10,
+    shadowOffset: {x : 5, y : 5},
+    shadowOpacity: 0.5
+}
 
 export class DrawablePoint extends Point {
     constructor(x, y) {
         super(x, y);
 
-        this.model = new Konva.Circle({
+        let conf = {
             x: x,
             y: y,
             radius: 5.5,
             stroke: 'red',
             strokeWidth: STROKE_WIDTH,
             draggable: true
-        });
+        };
+
+        $.extend(conf, SHADOW);
+
+        this.model = new Konva.Circle(conf);
 
         this.model.on('dragmove', () => this.updatePos());
     }
@@ -33,13 +43,17 @@ export class DrawableCircle extends Circle {
     constructor(center, diameter) {
         super(center, diameter);
 
-        this.model = new Konva.Circle({
-              x: center.x,
-              y: center.y,
-              radius: diameter / 2.0,
-              stroke: 'yellow',
-              strokeWidth: STROKE_WIDTH
-        });
+        let conf = {
+            x: center.x,
+            y: center.y,
+            radius: diameter / 2.0,
+            stroke: 'yellow',
+            strokeWidth: STROKE_WIDTH
+        };
+
+        $.extend(conf, SHADOW);
+
+        this.model = new Konva.Circle(conf);
     }
 }
 
@@ -52,13 +66,16 @@ export class DrawableParallelogram extends Parallelogram {
 
         for(let i = 0; i<this.edges.length; ++i)
         {
-            let line = new Konva.Line({
-                  points: [this.edges[i].p1.x, this.edges[i].p1.y, this.edges[i].p2.x, this.edges[i].p2.y],
-                  stroke: 'blue',
-                  strokeWidth: STROKE_WIDTH,
-                  lineCap: 'round',
-                  lineJoin: 'round'
-                });
+            let conf = {
+                points: [this.edges[i].p1.x, this.edges[i].p1.y, this.edges[i].p2.x, this.edges[i].p2.y],
+                stroke: 'blue',
+                strokeWidth: STROKE_WIDTH,
+                lineCap: 'round'
+            };
+
+            $.extend(conf, SHADOW);
+
+            let line = new Konva.Line(conf);
             this.model.add(line);
         }
     }
